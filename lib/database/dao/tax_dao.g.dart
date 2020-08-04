36fd361c3190908a87584d745311f2246a700443
devalp-pro <devalp.pro@gmail.com>
2020-08-04 18:36:36 +0530

    Solving product detail issues

---
+++ lib/database/dao/tax_dao.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tax_dao.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$TaxDaoMixin on DatabaseAccessor<AppDatabase> {
+  $ProductTaxTable get productTax => attachedDatabase.productTax;
+  $ProductsTable get products => attachedDatabase.products;
+}
